c593b774be17a4a6a26cbb2cd05594f9b2ef6649
Weyman Fung <wfung@pivotal.io>
2018-10-04 08:59:22 -0700

    Fix compile errors
    Signed-off-by: Tim Hausler <thausler@pivotal.io>

--- src/JsonSerializer.java
+++ src/JsonSerializer.java
@@ -1,12 +1,13 @@
-import java.util.*;
-import java.lang.reflect.Field;
 import java.io.*;
+import java.lang.reflect.Field;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class JsonSerializer {
 
   public String serialize(Object object) throws JsonSerializeException {
     try {
-      Class<?> objectClass = requireNonNull(object).getClass();
+      Class<?> objectClass = Objects.requireNonNull(object).getClass();
       Map<String, String> jsonElements = new HashMap<>();
 
       for (Field field : objectClass.getDeclaredFields()) {
@@ -18,23 +19,25 @@ public class JsonSerializer {
 
       System.out.println(toJsonString(jsonElements));
       return toJsonString(jsonElements);
-    } catch (IllegalArgumentException e) {
+    } catch (IllegalAccessException e) {
       throw new JsonSerializeException(e.getMessage());
     }
   }
 
   private String toJsonString(Map<String, String> jsonMap) {
-    String elementsString = jsonMap.entrySet()
-        .stream()
-        .map(entry -> "\"" + entry.getKey() + "\":\"" + entry.getValue() + "\"")
-        .collect(joining(","));
+    String elementsString =
+        jsonMap
+            .entrySet()
+            .stream()
+            .map(entry -> "\"" + entry.getKey() + "\":\"" + entry.getValue() + "\"")
+            .collect(Collectors.joining(","));
     return "{" + elementsString + "}";
   }
 
   private static String getSerializedKey(Field field) {
     String annotationValue = field.getAnnotation(JsonField.class).value();
 
-    if (annotationValue.isEmtpy()) {
+    if (annotationValue.isEmpty()) {
       return field.getName();
     } else {
       return annotationValue;
